9e0661e2eb46df4cfabecf0528a6bf236bdb682c
Nintendocustom <nintendocustomyt@gmail.com>
2026-09-13 20:07:51 +0000

    fix copy headers action

---
+++ nnheaders/nn/atk.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <nn/types.h>
+
+namespace nn {
+namespace atk {
+
+class SoundArchive {
+public:
+    struct SequenceSoundInfo;
+
+    const char* GetItemLabel(u32 id) const;
+    u32 GetItemId(const char* label) const;
+};
+
+class SoundActor  // Inherits SoundStartable, size: 0x7C
+{
+public:
+    virtual ~SoundActor();
+    u8 data[0x7C - 0x4];
+};
+
+}  // namespace atk
+}  // namespace nn
